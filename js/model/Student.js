a2dc1df03c7b2947601d934e7a3d3bcc990bd33b
nguyenkhaphuong <phuongkha1506@gmail.com>
2023-12-22 16:33:23 +0700

    Add 'english' to the 'Student' model

--- js/model/Student.js
+++ js/model/Student.js
@@ -1,10 +1,11 @@
 class Student {
-  constructor(_id, _name, _email, _math, _physics, _chemistry) {
+  constructor(_id, _name, _email, _math, _physics, _chemistry, _english) {
     this.id = _id
     this.name = _name
     this.email = _email
     this.math = _math
     this.physics = _physics
     this.chemistry = _chemistry
+    this.english = _english
   }
 }
