60d154be96e7c91f06cdcb04aa2833c734ea9ada
nguyenkhaphuong <phuongkha1506@gmail.com>
2023-12-22 16:06:03 +0700

    Update Student Model Class

---
+++ js/model/Student.js
@@ -0,0 +1,10 @@
+class Student {
+  constructor(_id, _name, _email, _math, _physics, _chemistry) {
+    this.id = _id
+    this.name = _name
+    this.email = _email
+    this.math = _math
+    this.physics = _physics
+    this.chemistry = _chemistry
+  }
+}
